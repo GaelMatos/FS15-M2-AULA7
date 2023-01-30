89248569d64509de7f255830c2979fa544f61728
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-30 09:52:58 -0300

    trabalho array

---
+++ scripts.js
@@ -0,0 +1,31 @@
+//let listaDeCompras = ["ovos","leite","manteiga","farinha"];
+//let listaDeCompras = [];
+//listaDeCompras [0] = "ovos";
+//listaDeCompras [0] = "leite";
+//listaDeCompras [0] = "manteiga";
+//listaDeCompras [200] = "farinha";
+
+//let listaDeCompras = [];
+//listaDeCompras.push("açucar");
+//listaDeCompras.push("feijão");
+//listaDeCompras.push("arroz");
+
+
+//let tentativas = 0;
+//let numeroRondonico = math.floor(math.random() * 100) +1;
+//function checartentativa(){
+   // tentativas++;
+
+    //if(tentativas == 10){
+    //    alert("GAME OVER");
+     //   perdeu();
+     //   return;
+    //}
+
+    //alert(tentativas);
+
+//}
+
+//function perdeu(){
+   // window.location.reload();
+//}
